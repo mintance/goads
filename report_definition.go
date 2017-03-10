38fa02ff85921086d91adcaffa3ddc541e8b61a0
Bohdan <www.lbohdan@gmail.com>
2017-03-10 13:41:01 +0200

    add to google reports definitions getiing keywords

--- report_definition.go
+++ report_definition.go
@@ -20,10 +20,13 @@ type ReportsRow struct {
 	Impressions string `json:"-impressions"`
 	Cost string `json:"-cost"`
 	Currency string `json:"-currency"`
+	Headline string `json:"-headline1"`
 	Date string `json:"-day"`
+	KeywordId string `json:"-keywordID"`
+	KeywordName string
 }
 
-type Reports struct {
+type AdReports struct {
 	Report struct {
 		ReportName struct {
 			Name string `json:"-name"`
@@ -43,6 +46,31 @@ type Reports struct {
 	} `json:"report"`
 }
 
+type KeywordReportsRow struct {
+	KeywordName string `json:"-keywordPlacement"`
+	KeywordId string `json:"-keywordID"`
+}
+
+type KeywordReports struct {
+	Report struct {
+		ReportName struct {
+			Name string `json:"-name"`
+		} `json:"report-name"`
+		DateRange struct {
+			Date string `json:"-date"`
+		} `json:"date-range"`
+		Table struct {
+			Columns struct {
+				Column []struct {
+					Name string `json:"-name"`
+					Display string `json:"-display"`
+				} `json:"column"`
+			} `json:"columns"`
+			Rows []KeywordReportsRow `json:"row"`
+		} `json:"table"`
+	} `json:"report"`
+}
+
 type ReportDefinitionService struct {
 	AuthConfig
 }
@@ -54,7 +82,7 @@ func NewReportDefinitionService(auth *AuthConfig) *ReportDefinitionService {
 
 func (s ReportDefinitionService) GetReport() []ReportsRow{
 
-	reqBody := []byte(`__rdxml=<?xml version="1.0" encoding="UTF-8"?>
+	adReports := []byte(`__rdxml=<?xml version="1.0" encoding="UTF-8"?>
 				<reportDefinition>
 				<selector>
 					<fields>CampaignId</fields>
@@ -65,6 +93,8 @@ func (s ReportDefinitionService) GetReport() []ReportsRow{
 					<fields>Cost</fields>
 					<fields>Date</fields>
 					<fields>AccountCurrencyCode</fields>
+					<fields>CriterionId</fields>
+					<fields>HeadlinePart1</fields>
 				</selector>
 				<reportName>Mintance</reportName>
 				<reportType>AD_PERFORMANCE_REPORT</reportType>
@@ -72,10 +102,34 @@ func (s ReportDefinitionService) GetReport() []ReportsRow{
 				<downloadFormat>XML</downloadFormat>
 				</reportDefinition>`)
 
-	return s.request(reqBody)
+	adKeywords := []byte(`__rdxml=<?xml version="1.0" encoding="UTF-8"?>
+				<reportDefinition>
+				<selector>
+					<fields>Id</fields>
+					<fields>Criteria</fields>
+				</selector>
+				<reportName>Mintance Keyword</reportName>
+				<reportType>CRITERIA_PERFORMANCE_REPORT</reportType>
+				<dateRangeType>TODAY</dateRangeType>
+				<downloadFormat>XML</downloadFormat>
+				</reportDefinition>`)
+
+	keywords := s.requestAdKeywords(adKeywords)
+
+	adverts := s.requestAdReport(adReports)
+
+	for _, keyword := range keywords{
+
+		for key := range adverts {
+			if keyword.KeywordId == adverts[key].KeywordId {
+				adverts[key].KeywordName = keyword.KeywordName
+			}
+		}
+	}
+	return adverts
 }
 
-func (s ReportDefinitionService) request(reqBody []byte) []ReportsRow{
+func (s ReportDefinitionService) requestAdReport(reqBody []byte) []ReportsRow{
 
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", "https://adwords.google.com/api/adwords/reportdownload/v201609", bytes.NewReader(reqBody))
@@ -108,9 +162,50 @@ func (s ReportDefinitionService) request(reqBody []byte) []ReportsRow{
 			"CustomerId": s.Auth.CustomerId,
 		}, "error","[Reports]: Convert xml to json error", err.Error())
 	}
-	reports := Reports{}
+	reports := AdReports{}
 
 	json.Unmarshal([]byte(reportsJson.String()), &reports)
 
 	return  reports.Report.Table.Rows
+}
+
+func (s ReportDefinitionService) requestAdKeywords(reqBody []byte) []KeywordReportsRow{
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "https://adwords.google.com/api/adwords/reportdownload/v201609", bytes.NewReader(reqBody))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	contentLength := fmt.Sprintf("%d", len(reqBody))
+	req.Header.Add("Content-length", contentLength)
+	req.Header.Add("developerToken", s.Auth.DeveloperToken)
+	req.Header.Add("clientCustomerId", s.Auth.CustomerId)
+	req.Header.Add("Authorization", "Bearer "+s.GetAccessToken())
+	req.Header.Add("Content-Type", "application/xml; charset=utf-8")
+
+	if err != nil {
+		mintance.Log(logrus.Fields{
+			"CustomerId": s.Auth.CustomerId,
+		}, "error","[Reports]: New request error", err.Error())
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		mintance.Log(logrus.Fields{
+			"CustomerId": s.Auth.CustomerId,
+		}, "error","[Reports]: Do request error", err.Error())	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+
+	reportsJson, err := xj.Convert(strings.NewReader(string(respBody)))
+
+	if err != nil {
+		mintance.Log(logrus.Fields{
+			"CustomerId": s.Auth.CustomerId,
+		}, "error","[Reports]: Convert xml to json error", err.Error())
+	}
+	reports := KeywordReports{}
+
+	json.Unmarshal([]byte(reportsJson.String()), &reports)
+
+	return reports.Report.Table.Rows
+
 }
